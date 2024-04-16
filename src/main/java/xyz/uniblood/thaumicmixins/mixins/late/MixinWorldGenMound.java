5c5abd628fb93394f87212db9743f016757e0d3a
Unicornbl00d <unicornbloods@protonmail.com>
2024-04-16 14:22:48 -0500

    Code for MixinWorldGenMound from nessie

--- src/main/java/xyz/uniblood/thaumicmixins/mixins/late/MixinWorldGenMound.java
+++ src/main/java/xyz/uniblood/thaumicmixins/mixins/late/MixinWorldGenMound.java
@@ -1,21 +1,27 @@
 package xyz.uniblood.thaumicmixins.mixins.late;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.gen.feature.WorldGenerator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Slice;
 import thaumcraft.common.lib.world.WorldGenMound;
 
 @Mixin(WorldGenMound.class)
 public abstract class MixinWorldGenMound extends WorldGenerator {
 
     @ModifyVariable(
-            method = "generate2()",
-            at = @At(value = "INVOKE_ASSIGN", target = "thaumcraft/common/lib/world/Dummy;generate2()D"),
-            index = 1
+            slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=dungeonChest")),
+            method = "generate2",
+            at = @At("STORE"),
+            print = true,
+            ordinal = 3,
+            index = 10,
+            name = "md"
     )
-    private int md1(int original) {
-        return 1;
+    private int md1(int original,  @Local(ordinal = 0, index = 9, name = "rr") float rr) {
+        return rr < 0.33F?1:0;
     }
 
 }
