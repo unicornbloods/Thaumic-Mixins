76f2139336de7e774468f375c0d91e861172daed
rndmorris <13648552+rndmorris@users.noreply.github.com>
2024-08-22 18:03:09 -0500

    Add full-reset to forgetResearch action

--- src/main/java/xyz/uniblood/thaumicmixins/commands/actions/ActionForgetResearch.java
+++ src/main/java/xyz/uniblood/thaumicmixins/commands/actions/ActionForgetResearch.java
@@ -18,6 +18,7 @@ import thaumcraft.common.lib.research.ResearchManager;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -31,10 +32,15 @@ public class ActionForgetResearch extends CommandAction
     public static final String MISSING_RESEARCH_KEY = "commands.tmixins.forgetresearch.doesnotknow";
     public static final String SUCCESS_KEY = "commands.tmixins.forgetresearch.success";
     public static final String NOTIFY_KEY = "commands.tmixins.forgetresearch.notify";
+    public static final String SUCCESS_ALL_KEY = "commands.tmixins.forgetresearch.successall";
+    public static final String NOTIFY_ALL_KEY = "commands.tmixins.forgetresearch.notifyall";
 
     public static final int ARG_INDEX_TARGET_PLAYER = 1;
     public static final int ARG_INDEX_RESEARCH_KEY = 2;
     public static final int ARG_INDEX_REFUND_STICKY = 3;
+    public static final String ALL_RESEARCH = "*";
+    public static final String TRUE = "true";
+    public static final String FALSE = "false";
 
     public ActionForgetResearch(CommandBase owningCommand)
     {
@@ -59,12 +65,20 @@ public class ActionForgetResearch extends CommandAction
             throw new WrongUsageException(USAGE_KEY);
         }
         final var targetPlayer = argTargetPlayer(sender, args);
+        final var allResearch = ALL_RESEARCH.equals(args[ARG_INDEX_RESEARCH_KEY]);
+        final var refundStickyWarp = argRefundStickyWarp(sender, args);
+
+        if (allResearch) {
+            forgetAllResearch(sender, targetPlayer, refundStickyWarp);
+            return;
+        }
+
         final var targetResearch = argTargetResearch(args);
         if (targetResearch == null) {
             sendErrorMessage(sender, NONEXISTENT_RESEARCH_KEY, args[ARG_INDEX_RESEARCH_KEY]);
             return;
         }
-        final var refundStickyWarp = argRefundStickyWarp(sender, args);
+
         forgetResearchAndRelated(sender, targetPlayer, targetResearch, refundStickyWarp);
     }
 
@@ -84,13 +98,15 @@ public class ActionForgetResearch extends CommandAction
             if (player == null) {
                 return Collections.emptyList();
             }
-            return CommandBase.getListOfStringsFromIterableMatchingLastWord(
-                args,
-                new ArrayList<>(getResearchKeys(player.getCommandSenderName()))
-            );
+            final var options = new ArrayList<>(Thaumcraft.proxy
+                .getPlayerKnowledge()
+                .researchCompleted
+                .get(player.getCommandSenderName()));
+            options.add(ALL_RESEARCH);
+            return CommandBase.getListOfStringsFromIterableMatchingLastWord(args, options);
         }
         if (args.length == ARG_INDEX_REFUND_STICKY + 1) {
-            return CommandBase.getListOfStringsMatchingLastWord(args, "true", "false");
+            return CommandBase.getListOfStringsMatchingLastWord(args, TRUE, FALSE);
         }
         return Collections.emptyList();
     }
@@ -116,13 +132,6 @@ public class ActionForgetResearch extends CommandAction
         return CommandBase.parseBoolean(sender, args[ARG_INDEX_REFUND_STICKY]);
     }
 
-    private List<String> getResearchKeys(String playerName) {
-        return Thaumcraft.proxy
-            .getPlayerKnowledge()
-            .researchCompleted
-            .get(playerName);
-    }
-
     private void forgetResearchAndRelated(ICommandSender sender, EntityPlayerMP targetPlayer, ResearchItem rootResearch, boolean refundStickyWarp) {
         final var playerName = targetPlayer.getCommandSenderName();
         final var researchName = rootResearch.getName();
@@ -131,8 +140,36 @@ public class ActionForgetResearch extends CommandAction
             return;
         }
 
+        final var queue = new LinkedList<String>();
+        queue.add(rootResearch.key);
+        forgetFromQueue(targetPlayer, refundStickyWarp, queue);
+
+        sendSuccessMessage(sender, SUCCESS_KEY, playerName, researchName);
+        sendColoredMessage(EnumChatFormatting.DARK_PURPLE, targetPlayer, NOTIFY_KEY, sender.getCommandSenderName(), researchName);
+    }
+
+    // Yes, this exists in the base TC4 command. The base TC4 command doesn't have proper autocompletion right now. This does.
+    private void forgetAllResearch(ICommandSender sender, EntityPlayerMP targetPlayer, boolean refundStickyWarp) {
+        final var playerName = targetPlayer.getCommandSenderName();
+        final var allPlayerResearch = new LinkedList<>(Thaumcraft.proxy.getPlayerKnowledge().researchCompleted.get(playerName));
+        forgetFromQueue(targetPlayer, refundStickyWarp, allPlayerResearch);
+
+        sendSuccessMessage(sender, SUCCESS_ALL_KEY, playerName);
+        sendColoredMessage(EnumChatFormatting.DARK_PURPLE, targetPlayer, NOTIFY_ALL_KEY, sender.getCommandSenderName());
+    }
+
+    private void forgetFromQueue(EntityPlayerMP targetPlayer, boolean refundStickyWarp, Deque<String> forgetQueue) {
+        final var playerName = targetPlayer.getCommandSenderName();
+
         // This should always be a reference to the exact list TC4 uses in-memory
-        final var knownResearchKeys = getResearchKeys(playerName);
+        final var knownResearchKeys = Thaumcraft.proxy
+            .getPlayerKnowledge()
+            .researchCompleted
+            .get(playerName);
+
+        if (knownResearchKeys == null) {
+            return;
+        }
 
         // Used for looking up sibling and descendant research
         final var knownResearchData = knownResearchKeys.stream()
@@ -141,11 +178,7 @@ public class ActionForgetResearch extends CommandAction
             .filter(Objects::nonNull)
             .collect(Collectors.toMap(r -> r.key, r -> r));
 
-        final var forgetQueue = new LinkedList<String>();
-        final var hasBeenQueued = new HashSet<String>();
-
-        forgetQueue.add(rootResearch.key);
-        hasBeenQueued.add(rootResearch.key);
+        final var hasBeenQueued = new HashSet<>(forgetQueue);
 
         var permanentWarpRefund = 0;
         var stickyWarpRefund = 0;
@@ -187,9 +220,6 @@ public class ActionForgetResearch extends CommandAction
             adjustStickyWarp(targetPlayer, stickyWarpRefund);
         }
         ResearchManager.scheduleSave(targetPlayer);
-
-        sendSuccessMessage(sender, SUCCESS_KEY, playerName, researchName);
-        sendColoredMessage(EnumChatFormatting.DARK_PURPLE, targetPlayer, NOTIFY_KEY, sender.getCommandSenderName(), researchName);
     }
 
     private static boolean isRelated(ResearchItem item, String query) {
